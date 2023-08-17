ea213f4d307932bd59e78f337a5a382940a88d5f
nathanzmacedo <nathanzmacedo@gmail.com>
2023-08-17 17:35:12 -0300

    svgs

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -1,6 +1,8 @@
 import React,{useState} from 'react'
 import "./Notes.css"
 import AddNew from '../components/UI/AddNew'
+import trash from '../svgs/trash.svg'
+import edit from '../svgs/edit.png'
 
 function Notes() {
   const [notes,setNotes] = useState([{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023", id: 1},{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023", id: 1},{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023", id: 1},{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023", id: 1},{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023", id: 1},{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023", id: 1},{title: "Muito legal",description: "eu amei one piece",date: "08/09/2023", id: 1},])
@@ -13,8 +15,8 @@ function Notes() {
                         <h1>{n.title}</h1>
                         <p className='desc'>{n.description}</p>
                         <div> 
-                          <div>edit</div>
-                          <div>excluir</div>
+                          <div><img src={edit} alt="" /></div>
+                          <div><img src={trash} alt="" /></div>
                         </div>
                       </div>
         )
